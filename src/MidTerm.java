4b5d05342988d27a3d95aa6d75cd10fa5e27895b
supriya <supriya7458@gmail.com>
2020-10-29 11:15:53 -0400

    change monkeys to students

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,11 +3,11 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println(
-				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork");
+				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja students.");
+		System.out.println("They were very clever students indeed, known for their ingenious teamwork");
 		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs.");
 		System.out.println("But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes.");
-		System.out.println("One-by-one every last pesky bug fell at the quick hands of the monkeys.");
+		System.out.println("One-by-one every last pesky bug fell at the quick hands of the students.");
 	}
 
 }
