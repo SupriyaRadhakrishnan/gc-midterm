b3e87761a69b17b18d4fcdc9b95bc29f3b410a8a
timstetter <tstetter@gmail.com>
2020-10-29 11:34:08 -0400

    Deleted the story

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -2,21 +2,7 @@
 public class MidTerm {
 
 	public static void main(String[] args) {
-		System.out.println(
-
-				"Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students.");
-		System.out.println("They were very clever students indeed, known for their ingenious teamwork");
-		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs.");
-		System.out.println("But following David’s unifying leadership, they paired up and mowed down their foes.");
-		System.out.println("One-by-one every last pesky bug fell at the quick hands of the students.");
 		
-		String name = "Supriya";
-		System.out.println(name);
-
-
-		System.out.println("Supriya!");
-
-		System.out.println("Tim");
 
 	}
 
