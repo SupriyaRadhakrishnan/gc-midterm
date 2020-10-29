02e2320e3eb5bf131bd56bbf5f322ebfce43ec76
timstetter <tstetter@gmail.com>
2020-10-29 11:15:29 -0400

    Detroit and David

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,10 +3,10 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println(
-				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+				"Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja monkeys.");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork");
 		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs.");
-		System.out.println("But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes.");
+		System.out.println("But following David’s unifying leadership, they paired up and mowed down their foes.");
 		System.out.println("One-by-one every last pesky bug fell at the quick hands of the monkeys.");
 	}
 
